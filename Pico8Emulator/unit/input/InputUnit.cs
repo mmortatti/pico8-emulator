c8be85436059b20f43de5e3f8f762c5b98bc8d14
Matheus Mortatti <matheusmortatti@gmail.com>
2019-10-13 16:15:11 -0300

    added back bitmask to btn and btnp + added test cart for btn

--- Pico8Emulator/unit/input/InputUnit.cs
+++ Pico8Emulator/unit/input/InputUnit.cs
@@ -20,16 +20,19 @@ namespace Pico8Emulator.unit.input {
 		public override void DefineApi(LuaInterpreter script) {
 			base.DefineApi(script);
 
-			script.AddFunction("btn", (Func<int?, int?, bool>)Btn);
-			script.AddFunction("btnp", (Func<int?, int?, bool>)Btnp);
+			script.AddFunction("btn", (Func<int?, int?, object>)Btn);
+			script.AddFunction("btnp", (Func<int?, int?, object>)Btnp);
 		}
 
 		public override void Update() {
 			base.Update();
 
-			for (var i = 0; i < ButtonCount; i++) {
-				_previousButtonState[i] = _currentButtonState[i];
-				_currentButtonState[i] = Emulator.InputBackend.IsButtonDown(i, 0);
+			for (var i = 0; i < ButtonCount; ++i) {
+				for (var p = 0; p < MaxPlayers; ++p) {
+					var index = ToIndex(i, p);
+					_previousButtonState[index] = _currentButtonState[index];
+					_currentButtonState[index] = Emulator.InputBackend.IsButtonDown(i, p);
+				}
 			}
 		}
 
@@ -37,12 +40,35 @@ namespace Pico8Emulator.unit.input {
 			return (i ?? 0) + ((p ?? 0) * ButtonCount);
 		}
 
-		public bool Btn(int? i = null, int? p = null) {
+		public object Btn(int? i = null, int? p = null) {
+			if (i == null) {
+				int bitMask = 0;
+				for (int k = 0; k < ButtonCount; ++k) {
+					for (int j = 0; j < 2; ++j) {
+						bitMask |= ((_currentButtonState[ToIndex(k, j)] ? 1 : 0) << (ButtonCount * j + k));
+					}
+				}
+				return bitMask;
+			}
+
 			return _currentButtonState[ToIndex(i, p)];
 		}
 
-		public bool Btnp(int? i = null, int? p = null) {
-			var index = ToIndex(i, p);
+		public object Btnp(int? i = null, int? p = null) {
+			int index = 0;
+
+			if (i == null) {
+				int bitMask = 0;
+				for (int k = 0; k < ButtonCount; ++k) {
+					for (int j = 0; j < 2; ++j) {
+						index = ToIndex(k, j);
+						bitMask |= ((_currentButtonState[index] && !_previousButtonState[index] ? 1 : 0) << (ButtonCount * j + k));
+					}
+				}
+				return bitMask;
+			}
+
+			index = ToIndex(i, p);
 			return _currentButtonState[index] && !_previousButtonState[index];
 		}
 	}
